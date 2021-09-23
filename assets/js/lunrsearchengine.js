d8d2595423e9627131bd737e268a76740bcc723f
ChrisHammond <ChrisHammond@users.noreply.github.com>
2021-09-23 15:47:30 +0000

    deploy: 159b4cc193b7fc3d71b8acf2191d9f0cbd81a872

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,189 @@
+
+var documents = [{
+    "id": 0,
+    "url": "/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "/about/",
+    "title": "About TheTrashman.com",
+    "body": "This is a domain name. We are not affiliated with an actual trash or refuse business, but we could be! Do you want to lease this domain? "
+    }, {
+    "id": 2,
+    "url": "/about",
+    "title": "About TheTrashman.com",
+    "body": "I’ll wreck you "
+    }, {
+    "id": 3,
+    "url": "/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 4,
+    "url": "/",
+    "title": "Home",
+    "body": "{% if page. url == “/” %}       Featured:       {% for post in site. posts %}    {% if post. featured == true %}      {% include featuredbox. html %}    {% endif %}  {% endfor %}  {% endif %}       All Stories:         {% for post in paginator. posts %}    {% include postbox. html %}    {% endfor %}    {% include pagination. html %}"
+    }, {
+    "id": 5,
+    "url": "/videos",
+    "title": "TheTrashman.com Videos",
+    "body": "Videos coming soon "
+    }, {
+    "id": 6,
+    "url": "/About",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 7,
+    "url": "/Videos/Video-Player",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 8,
+    "url": "/Home/catpageid/2",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 9,
+    "url": "/Home/catpageid/3",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 10,
+    "url": "/Home/catpageid/4",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 11,
+    "url": "/Home/catpageid/5",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 12,
+    "url": "/Home/catpageid/6",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 13,
+    "url": "/Home/catpageid/7",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 14,
+    "url": "/Home/catpageid/8",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 15,
+    "url": "/Home/catpageid/9",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 16,
+    "url": "/Videos",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 17,
+    "url": "/redirects.json",
+    "title": "",
+    "body": "{“/About”:”/about”,”/Videos/Video-Player”:”/”,”/Home/catpageid/2”:”/”,”/Home/catpageid/3”:”/”,”/Home/catpageid/4”:”/”,”/Home/catpageid/5”:”/”,”/Home/catpageid/6”:”/”,”/Home/catpageid/7”:”/”,”/Home/catpageid/8”:”/”,”/Home/catpageid/9”:”/”,”/Videos”:”/videos”} "
+    }, {
+    "id": 18,
+    "url": "/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 19,
+    "url": "/thetrashman-simulation-racing",
+    "title": "TheTrashman.com crashing you in a simulation race, or real race, near you",
+    "body": "2021/09/23 - TheTrashman is back in Sim Racing, look out. Terrorizing drivers in iRacing, ACC and various other platforms. Long crash the TheTrashman. com: "
+    }, {
+    "id": 20,
+    "url": "/thetrashman-is-no-more",
+    "title": "TheTrashman.com is no more, long live TheTrashman.com",
+    "body": "2021/09/01 - Last night while playing Fortnite I received an email from Epic announcing that they had initiated a username change for my account, either because I had requested it, or I was found in violation of their username policies. After thoroughly checking the validity of the email, to ensure it wasn’t phishing, I figured out that someone must have reported my name as a violation. After logging in I saw that my username had been changed to User-somethingsomethingsomething. A few keystrokes later and my username was set back to TheTrashman. com Long live the TheTrashman. com: "
+    }, {
+    "id": 21,
+    "url": "/thetrashman-video-replay",
+    "title": "TheTrashman.com Rocket League Game Play!",
+    "body": "2020/12/21 - The latest replay video from TheTrashman. com Rocket League "
+    }, {
+    "id": 22,
+    "url": "/thetrashman-com-in-rocket-league",
+    "title": "TheTrashman.com in Rocket League!",
+    "body": "2020/12/06 - TheTrashman. com is a proud sponsor of Chris Hammond’s eSports gaming efforts. You can currently find him playing in competitive Rocket League Matches. You can also find him playing Solo/Duals/Trios/Squads in Fortnite. Did you get beat by TheTrashman. com? Did you beat him? Let us know! "
+    }, {
+    "id": 23,
+    "url": "/welcome-to-thetrashman-com",
+    "title": "Welcome to TheTrashman.com!",
+    "body": "2020/11/16 - You’ve found a great domain that is for sale. If you have a trash or refuse business, or any other type of business this domain might be perfect for you. If you are interested in purchasing the domain fill out the contact form on this page. Include your best offer for the domain. We will respond to your offer, payment methods are Paypal, or cash ($$ US Dollars) in St. Louis, Missouri Low ball offers, and inquiries without an offer, will not be responded to. Login To Your Account: Trying to login to pay your trash bill? You’ve got the wrong site people. This is not your trash service. Fortnite: If you’re here because of Fortnite… Best of luck to you. Site Usage: Potential uses for this Domain: 12345Small Business WebsiteLarge Business WebsiteGamer WebsiteComedian WebsiteMuch much more!Small Business Ideas: 1234567Junk Recycling BusinessCommercial Trash ServicesCommercial Waste ServicesJunk Removal ServicesTrash Bin Rental ServicesDomain Listing and Retail ServicesDomain Collection and Recycle ServicesLarge Business Ideas: 123456Large Hawaii Based Trash CompanyJunk Recycling BusinessCommercial Trash ServicesCommercial Waste ServicesJunk Removal ServicesTrash Bin Rental Services"
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
